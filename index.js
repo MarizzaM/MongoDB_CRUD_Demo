02b23387e2c84f4901362006e4889f45b6d90244
Pavlova Marina <marizza.mil89@gmail.com>
2021-09-07 16:46:31 +0300

    03_Models

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected...'))
     .catch(err => console.error('Connection failed...', err));
 
+// Defining a schema 
 const courseSchema = new mongoose.Schema({
     name: String,
     author: String,
     tags: [String],
     date: { type: Date, default: Date.now },
     isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+const course = new Course({
+    name: 'Node.js Course',
+    author: 'Marizza',
+    tags: ['node', 'backend'],
+    isPublished: true
 });
