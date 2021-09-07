60fb350d1bcb2a9bca3d8c2a4163f7b45609ac3e
Pavlova Marina <marizza.mil89@gmail.com>
2021-09-07 22:06:08 +0300

    06_Updating_a_Document_QueryFirst

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const Course = mongoose.model('Course', courseSchema);
 async function createCourse(){
     const course = new Course({
         name: 'Angular Course',
-        author: 'Marizza',
+        author: 'MarizzaMil',
         tags: ['angular', 'frontend'],
         isPublished: true
     });
@@ -27,15 +27,28 @@ async function createCourse(){
     const result = await course.save();
     console.log(result);
 }
-
-createCourse();
+//createCourse();
 
 async function getCourses(){
+
     const courses = await Course
-        .find({author: 'Marizza',  isPublished: true})
+        .find({author: 'MarizzaMil',  isPublished: true})
         .limit(10)
-        .sort({name: 1});
+        .sort({name: 1})
+        .select({name: 1, tags: 1})
     console.log(courses);
 }
+//getCourses();
 
-getCourses();
+async function updateCourse(id){
+    const course = await Course.findById(id);
+    if (!course) return;
+
+    course.isPublished = true;
+    course.author = 'Another Author';
+
+    const result = await course.save();
+    console.log(result);
+}
+
+updateCourse('61376e270f24413b1e542da8');
