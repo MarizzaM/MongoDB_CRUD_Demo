d6c4be1822ff36b06474f37b58afa980929a5cb3
Pavlova Marina <marizza.mil89@gmail.com>
2021-09-07 16:29:39 +0300

    02_Schemas

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected...'))
     .catch(err => console.error('Connection failed...', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
